7413ea296cfbdbb7dd2a9573dffebc27d06bf686
emorast <egil.morast@gmail.com>
2021-10-24 15:34:29 +0200

    Starting out

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require ('express')
+
+const app = express()
+
+const port = 3000
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => res.sendFile(__dirname + '/geobasement.html'))
+
+app.listen(port, () => console.log(`App listening on port ${ port }! `))
---
+++ public/map.js
@@ -0,0 +1,8 @@
+
+var map = new maplibregl.Map({
+  container: 'map',
+  style: 'https://api.maptiler.com/maps/streets/style.json?key=VW2mYLpPUeqM7UzRMQES',
+  center: [17.995172990966378, 59.294739331660445],
+  zoom: 14
+
+});
