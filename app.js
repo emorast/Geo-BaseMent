017602e430916c6254a9007add26823c2221c709
emorast <egil.morast@gmail.com>
2021-10-26 15:42:02 +0200

    Mock-up

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const port = 3000
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/geobasement.html'))
+app.get('/', (req, res) => res.sendFile(__dirname + '/frontPage.html'))
+
+app.get('/application', (req, res) => res.sendFile(__dirname + '/geobasement.html'))
+
+app.get('/home', (req, res) => res.sendFile(__dirname + '/frontPage.html'))
 
 app.listen(port, () => console.log(`App listening on port ${ port }! `))
